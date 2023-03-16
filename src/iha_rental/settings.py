6fc3bc137d5c742eee4f5d168aad0150a35839bf
Ahmet Hakan Özgüven <99harkan@gmail.com>
2023-03-16 11:11:22 +0300

    final

--- src/iha_rental/settings.py
+++ src/iha_rental/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
 
 
 # Application definition
